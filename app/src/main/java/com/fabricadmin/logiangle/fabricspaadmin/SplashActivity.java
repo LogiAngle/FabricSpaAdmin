f63b5595884ffe8c5fdf51354883811e2be9991a
LogiAngle <santoshrp@togethersolutions.in>
2018-12-27 18:12:01 +0530

    print Statement  with exampledfsfsdf

--- app/src/main/java/com/fabricadmin/logiangle/fabricspaadmin/SplashActivity.java
+++ app/src/main/java/com/fabricadmin/logiangle/fabricspaadmin/SplashActivity.java
@@ -10,6 +10,6 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        System.out.println("Santosh x hj jkhk here");
+        System.out.println("Santosh hj jkhk here");
     }
 }
