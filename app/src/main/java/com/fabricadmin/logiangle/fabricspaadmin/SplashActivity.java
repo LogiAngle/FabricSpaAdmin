da1f095cd9af2de2099e0b1907364ca20c50af0e
LogiAngle <santoshrp@togethersolutions.in>
2018-12-27 18:21:51 +0530

    Print Statement is commented from splash activity

--- app/src/main/java/com/fabricadmin/logiangle/fabricspaadmin/SplashActivity.java
+++ app/src/main/java/com/fabricadmin/logiangle/fabricspaadmin/SplashActivity.java
@@ -10,6 +10,6 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        System.out.println("ABC here");
+      //  System.out.println("ABC here");
     }
 }
