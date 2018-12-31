845c26daf092690549505a57b29c7995992c0343
LogiAngle <santoshrp@togethersolutions.in>
2018-12-31 17:14:47 +0530

    Merge remote-tracking branch 'remotes/origin/Sujit'
    # Conflicts:
    #	app/src/main/java/com/fabricadmin/logiangle/fabricspaadmin/javaClasses/AllConstants.java

--- app/src/main/java/com/fabricadmin/logiangle/fabricspaadmin/javaClasses/AllConstants.java
+++ app/src/main/java/com/fabricadmin/logiangle/fabricspaadmin/javaClasses/AllConstants.java
@@ -4,4 +4,5 @@ public class AllConstants {
 
     public static final String LOGGEDIN_SHARED_PREF = "loggedin";
     public static final String KEY_LOGIN_ID = "loginid";
+    public static final String KEY_LOGIN_ID1 = "loginid";
 }
